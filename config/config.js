e09f82442c9d26c97f6bb4248f13aa79392005be
미운 오리 삼형제 <illinass@kakao.com>
2025-11-29 18:07:21 +0900

    update patchnote

---
+++ config/config.js
@@ -0,0 +1,7 @@
+// 웹사이트 설정 파일
+// 보안을 위해 이 파일을 .gitignore에 추가하는 것을 권장합니다.
+
+const CONFIG = {
+    // Discord Webhook URL - 버그 신고 및 제보용
+    DISCORD_WEBHOOK_URL: 'https://discordapp.com/api/webhooks/1442726767222718554/i3QtvZ6-giYqwtlheMm3ugxcJoVsoVYmExBPX2n-uEoFHpWwmGqaaO_f6gnlBNXCZYas'
+};
